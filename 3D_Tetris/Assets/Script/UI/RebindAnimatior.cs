a89233dac8c862b916a1e4a234a3221b7be9aaba
mporhunova <mporhunova@gkbaltbet.local>
2019-08-27 16:30:27 +0300

    change pause/start icon. add pause animation ( have time scale animation bug)

---
+++ 3D_Tetris/Assets/Script/UI/RebindAnimatior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RebindAnimatior : MonoBehaviour
+{
+	Animator _animator;
+	// Use this for initialization
+	void Start () {
+		_animator = GetComponent<Animator>();
+	}
+
+	void OnDisable() {
+		_animator.Rebind();
+	}
+}
---
+++ 3D_Tetris/Assets/Script/UI/WaitBeforeEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaitBeforeEvent : MonoBehaviour
+{
+	[SerializeField] float _Timer;
+	[SerializeField] UnityEvent _InvokeAfterTime;
+
+	void Start() {
+		StartCoroutine(Timer());
+	}
+
+	IEnumerator Timer() {
+		yield return new WaitForSeconds( _Timer);
+		_InvokeAfterTime.Invoke();
+	}
+}
