e01ab41f082c5ca49a436b8e2431a237544d7ee8
mporhunova <mporhunova@gkbaltbet.local>
2019-09-18 17:12:21 +0300

    start change editor

--- 3D_Tetris/Assets/Script/StateMachine/Editor/StateMachineEditor.cs
+++ 3D_Tetris/Assets/Script/StateMachine/Editor/StateMachineEditor.cs
@@ -12,15 +12,23 @@ public class StateMachineEditor : Editor {
     SerializedProperty table;
     SerializedProperty text1;
     int _needCount;
-
+    bool showPosition;
+    EMachineState _key;
+    Dictionary<StateMachine, List<EMachineState>> States = new Dictionary<StateMachine, List<EMachineState>>();
     private void OnEnable() {
         table = serializedObject.FindProperty("_StateTable");
         text1 = serializedObject.FindProperty("_UIText");
         _needCount = Enum.GetValues(typeof(EMachineState)).Length ;
 
         component = (StateMachine)target;
+        
+        
     }
 
+    InitializationStates() {
+        
+    }
+    
     public override void OnInspectorGUI() {
 
         serializedObject.Update();
@@ -32,20 +40,53 @@ public class StateMachineEditor : Editor {
 
         for (int row = 0; row < _needCount; row++) {
 
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(((EMachineState)row).ToString());
-            for (int column = 0; column < _needCount; column++) {
+            EditorGUILayout.LabelField(((EMachineState)row).ToString(), EditorStyles.boldLabel);
+            EditorGUI.indentLevel++;
+            OneStateInGUI(  (EMachineState)row);
+//            EditorGUILayout.BeginHorizontal();
+//            EditorGUILayout.LabelField(((EMachineState)row).ToString());
+//            for (int column = 0; column < _needCount; column++) {
+//
+//               var yach = table.GetArrayElementAtIndex(row * _needCount + column);
+//                yach.boolValue = GUILayout.Toggle(yach.boolValue,"");
+//            }
+//            EditorGUILayout.EndHorizontal();
+            EditorGUI.indentLevel--;
+            if(GUILayout.Button("Add state connect"))
+            {
+                Debug.Log(" empty. add item connect");
+            }
+        }
+        
+       
+        serializedObject.ApplyModifiedProperties();
+    }
+    
+    /*
+     * for (int column = 0; column < _needCount; column++) {
 
                var yach = table.GetArrayElementAtIndex(row * _needCount + column);
                 yach.boolValue = GUILayout.Toggle(yach.boolValue,"");
             }
-            EditorGUILayout.EndHorizontal();
-        }
-        
-        serializedObject.ApplyModifiedProperties();
-    }
+     */
 
     private void ChangeTable() {
         table.arraySize = _needCount* _needCount;      
+    }
+
+    public void OneStateInGUI( EMachineState state) {
+        
+        EditorGUILayout.BeginHorizontal();
+        _key = (EMachineState) EditorGUILayout.EnumPopup("state", _key);
+        
+        if(GUILayout.Button("(-) delete"))
+        {
+            Debug.Log(" empty. delete item");
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void DeleteStateConnection() {
+        
     }
 }
