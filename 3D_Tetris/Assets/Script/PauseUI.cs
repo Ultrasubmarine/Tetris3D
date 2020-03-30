5a9b5dc0a92f157ef7a5c2227f94bfaf3321c656
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-03-30 19:57:31 +0300

    Fix pause;

--- 3D_Tetris/Assets/Script/PauseUI.cs
+++ 3D_Tetris/Assets/Script/PauseUI.cs
@@ -1,49 +1,17 @@
-﻿using UnityEngine;
+﻿using Script.Influence;
+using Script.UI;
+using UnityEngine;
 
 public class PauseUI : MonoBehaviour
 {
-    private Animator _animator;
-
-    private float _lastTimeScale;
+    [SerializeField] private PausePanel _pausePanel;
 
     
-    private const string _show = "Show";
-    
-    private const string _hide = "Hide";
-    
-    
-    private void Awake()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
-    public void Close()
-    {
-        Time.timeScale = _lastTimeScale;
-        gameObject.SetActive(false);
-    }
+    [SerializeField] private GameObject _pauseIcon;
 
     public void SetPauseGame(bool isPause)
     {
-        if(isPause)
-            Show();
-        else
-            Hide();
-    }
-
-    
-    private void Show()
-    {
-        _lastTimeScale = Time.timeScale;
-        Time.timeScale = 0;
-        
-        gameObject.SetActive(true);
-        
-        _animator.Play(_show);    
-    }
-
-    private void Hide()
-    {
-        _animator.Play(_hide);
+        _pauseIcon.SetActive(!isPause);
+        _pausePanel.SetPauseGame(isPause);
     }
 }
