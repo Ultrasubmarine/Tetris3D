e967bea893ef62ee7b7ef79f37275fb0bfacfc5f
MarinaPorkhunova <MarinaPorkhunova@MyTona.com>
2020-04-27 22:29:42 +0300

    Fixed pause & bottom panel;

---
+++ 3D_Tetris/Assets/Script/Animations/BottomPanelAnimation.cs
@@ -0,0 +1,47 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+[RequireComponent(typeof(CanvasGroup))]
+[RequireComponent(typeof(RectTransform))]
+public class BottomPanelAnimation : MonoBehaviour
+{
+    private CanvasGroup _canvasGroup;
+
+    private RectTransform _rectTransform;
+    
+    [SerializeField] private float _time;
+
+    [SerializeField] private float _yMove;
+
+    private Sequence _show;
+    private Sequence _hide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+        _rectTransform = GetComponent<RectTransform>();
+
+        _show = DOTween.Sequence().SetAutoKill(false).Pause();
+        _show.Append(_canvasGroup.DOFade(1, _time / 2).From(0))
+            .Join(_rectTransform.DOAnchorPosY(0, _time / 2).From(Vector2.up * _yMove));//From(125,false))
+        
+        _hide = DOTween.Sequence().SetAutoKill(false).Pause();
+        _hide.Append(_canvasGroup.DOFade(0, _time / 2).From(1))
+            .Join(_rectTransform.DOAnchorPosY(_yMove, _time / 2).From(Vector2.zero)).OnComplete( () => gameObject.SetActive(false));
+    }
+    
+    // Update is called once per frame
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        _show.Rewind();
+        _show.Play();
+    }
+
+    public void Hide()
+    {
+        _hide.Rewind();
+        _hide.Play();
+    }
+}
--- 3D_Tetris/Assets/Script/PauseUI.cs
+++ 3D_Tetris/Assets/Script/PauseUI.cs
@@ -1,16 +1,42 @@
-﻿using Script.UI;
+﻿using System;
+using Script.Influence;
+using Script.UI;
 using UnityEngine;
 
 public class PauseUI : MonoBehaviour
 {
     [SerializeField] private PausePanel _pausePanel;
 
+    [SerializeField] private BottomPanelAnimation _pausePanel2;
+    
+    [SerializeField] private BottomPanelAnimation _gamePanel;
     
     [SerializeField] private GameObject _pauseIcon;
 
+    private InfluenceManager _influenceManager;
+
+    private void Awake()
+    {
+        _influenceManager = RealizationBox.Instance.influenceManager;
+    }
+
     public void SetPauseGame(bool isPause)
     {
-        _pauseIcon.SetActive(!isPause);
-        _pausePanel.SetPauseGame(isPause);
+        /*_pauseIcon.SetActive(!isPause);
+        _pausePanel.SetPauseGame(isPause);*/
+
+        _influenceManager.enabled = !isPause;
+        
+        if (isPause)
+        {
+            _pausePanel2.Show();
+            _gamePanel.Hide();
+        }
+        else
+        {
+            _gamePanel.Show();
+            _pausePanel2.Hide();
+        }
+        
     }
 }
