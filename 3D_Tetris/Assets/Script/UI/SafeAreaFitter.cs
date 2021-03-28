0139e1bc257b48bf244ef995f42e605ca699c5cc
marina porkhunova <marina.porkhunova@mytona.com>
2021-03-28 11:47:07 +0300

    ..??

---
+++ 3D_Tetris/Assets/Script/UI/SafeAreaFitter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour
+{
+    private RectTransform _rectTransform;
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+
+        var safeArea = Screen.safeArea;
+        var anchorMin = safeArea.position;
+        var anchorMax = safeArea.position + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+    }
+}
