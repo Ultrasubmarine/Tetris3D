b63305254c19c121f29d6b7eece2fc55e016bec5
sybmarina3000 <sybmarina3000@yandex.ru>
2022-05-23 12:30:50 +0300

    bud fix; bomb offer

--- 3D_Tetris/Assets/Script/PlayerProfile/InventoryUI.cs
+++ 3D_Tetris/Assets/Script/PlayerProfile/InventoryUI.cs
@@ -10,8 +10,6 @@ namespace Script.PlayerProfile
         private bool _isShow = false;
         [SerializeField] private BottomPanelAnimation _panel;
 
-        [SerializeField] private bool gamePlayState; // always shsow
-        
         //big bomb
         [SerializeField] private TextMeshProUGUI _bombAmountText;
         [SerializeField] private RectTransform _bombRect;
@@ -24,38 +22,13 @@ namespace Script.PlayerProfile
         private void Start()
         {
             SetBombAmountText();
-            if (!gamePlayState)
-            {
-                PlayerSaveProfile.instance.onBombAmountChange += AddBigBombInInventory;
+  
+            _applyBomb = DOTween.Sequence().SetAutoKill(false).Pause();
+            _applyBomb.Append(_bombRect.DOScale(Vector3.one * 1.3f, 0.3f).From(Vector3.one).OnComplete(()=>SetBombAmountText()))
+                .Append(_bombRect.DOScale(Vector3.one, _time / 2))
+                .OnComplete(() => UpdateVisibility());
                 
-                // todo fix this ugly code 
-                _panel.onShowEnded += () =>
-                {
-                    _addBomb.Rewind();
-                    _addBomb.Play();
-                };
-                
-                _addBomb= DOTween.Sequence().SetAutoKill(false).Pause();
-                _addBomb.AppendInterval(0.7f)
-                    .Append(_bombRect.DOScale(Vector3.one * 1.3f, 0.3f).From(Vector3.one).OnComplete(()=>SetBombAmountText()))
-                    .Append(_bombRect.DOScale(Vector3.one, _time / 2))
-                    .AppendInterval(0.2f)
-                    .OnComplete(() => _panel.Hide());
-            }
-            else
-            {
-             //   PlayerSaveProfile.instance.onBombAmountChange += UpdateVisibility;
-                
-                _applyBomb = DOTween.Sequence().SetAutoKill(false).Pause();
-                _applyBomb.Append(_bombRect.DOScale(Vector3.one * 1.3f, 0.3f).From(Vector3.one).OnComplete(()=>SetBombAmountText()))
-                    .Append(_bombRect.DOScale(Vector3.one, _time / 2))
-                    .OnComplete(() => UpdateVisibility());
-            }
-            
-            if (gamePlayState)
-            {
-                UpdateVisibility();
-            }
+            UpdateVisibility();
         }
 
         public void UseBigBomb()
@@ -69,13 +42,7 @@ namespace Script.PlayerProfile
                 RealizationBox.Instance.generator.SetNextAsBigBomb();
             }
         }
-        
-        public void AddBigBombInInventory(int amount)
-        {
-            _panel.Show();
-        }
 
-        
         public void UpdateVisibility(int amount = 0)
         {
             SetBombAmountText();
@@ -95,18 +62,6 @@ namespace Script.PlayerProfile
         public void SetBombAmountText()
         {
             _bombAmountText.text = "<size=60%>x</size=70%>" + PlayerSaveProfile.instance._bombAmount;
-        }
-        
-        private void OnDestroy()
-        {
-            if (!gamePlayState)
-            {
-                PlayerSaveProfile.instance.onBombAmountChange -= AddBigBombInInventory;
-            }
-            else
-            {
-           //     PlayerSaveProfile.instance.onBombAmountChange -= UpdateVisibility;
-            }
         }
 
     }
