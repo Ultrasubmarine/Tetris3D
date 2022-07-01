7e77b983926120570892ed878072c4fd828fc28d
sybmarina3000 <sybmarina3000@yandex.ru>
2022-07-01 20:22:02 +0300

    evil box animation;

--- 3D_Tetris/Assets/Script/GameLogic/EvilBoxManager.cs
+++ 3D_Tetris/Assets/Script/GameLogic/EvilBoxManager.cs
@@ -69,6 +69,7 @@ namespace Script.GameLogic
 
        private Sequence _uiBoxAnimation;
        [SerializeField] private RectTransform _evilBoxRectTransform;
+       [SerializeField] private RectTransform _evilBoxParentRectTransform;
        [SerializeField] private CanvasGroup _oreol;
        [SerializeField] private float _time = 0.7f;
        [SerializeField] private float _deltaMove;
@@ -76,6 +77,13 @@ namespace Script.GameLogic
        private float _rotation;
        [SerializeField] private RectTransform _oreolTransform;
 
+       
+       // 3d animation
+       [SerializeField] private Canvas _canvas;
+       [SerializeField] private Transform _centerPoint;
+       [SerializeField] private float _timeBlockmove = 1f;
+       private int _blockCompleteCounter;
+       
 
        [SerializeField] private int _minBox, _maxBox;
        private void Start()
@@ -96,20 +104,21 @@ namespace Script.GameLogic
 
             CanvasGroup boxCanvas = _evilBoxRectTransform.gameObject.GetComponent<CanvasGroup>();
             _uiBoxAnimation = DOTween.Sequence().SetAutoKill(false).Pause();
-            _uiBoxAnimation 
-                .Append(_evilBoxRectTransform.DOAnchorPosY(Screen.height / 2 + 100, _time / 2).From(Vector2.up * (Screen.height / 2 - 300)))
+            _uiBoxAnimation
+                .Append(_evilBoxRectTransform.DOAnchorPosY(170, _time / 2)
+                    .From(Vector2.up * (-200)))
                 .Join(boxCanvas.DOFade(1f, _time / 2.5f).From(0f))
-                .Append(_evilBoxRectTransform.DOAnchorPosY(Screen.height / 2 , _timeMoving)
-                    .From(Vector2.up *(Screen.height / 2 + 100)).SetLoops(3, LoopType.Yoyo))
-                .Join(_oreol.DOFade(1,_timeMoving/4 ).From(0).OnComplete(() =>
+                .Append(_evilBoxRectTransform.DOAnchorPosY(130, _timeMoving)
+                    .From(Vector2.up * (170)).SetLoops(3, LoopType.Yoyo))
+                .Join(_oreol.DOFade(1, _timeMoving / 4).From(0).OnComplete(() =>
                 {
                     OpenEvilBoxLogic();
                 }))
                 //HIDE PART
-                .Append(_oreol.DOFade(0,_time/4 ).From(1))
-                .Append(_evilBoxRectTransform.DOAnchorPosY(Screen.height / 2 + 100, _time/3))
-                .Join(boxCanvas.DOFade(0f, _time/3).From(1f))
-                .OnComplete( ()=>OnOpenBoxEnded?.Invoke());
+                .Append(_oreol.DOFade(0, _time / 4).From(1))
+             //   .Append(_evilBoxRectTransform.DOAnchorPosY(_evilBoxRectTransform.anchoredPosition.y + 100, _time / 3))
+                .Join(boxCanvas.DOFade(0f, _time / 3).From(1f));
+               // .OnComplete( ()=>OnOpenBoxEnded?.Invoke());
 
             _uiBoxAnimation.OnUpdate(() =>
             {
@@ -224,8 +233,13 @@ namespace Script.GameLogic
         {
             if (_isOpenedBox ==0)
                 return false;
-             
             
+            int _minHeight = 17;
+            int currentHeightPosition  = (_matrix.height - _minHeight) * _gameCamera.lastMaxCurrentHeight / _heightHandler.limitHeight + _minHeight; //(_matrix.height - _minHeight) * _heightHandler.currentHeight / _heightHandler.limitHeight + _minHeight;
+            currentHeightPosition -= 7;
+            _centerPoint.position = new Vector3(0, currentHeightPosition, 0);
+            
+            SetUIBox(_centerPoint.position);
             _uiBoxAnimation.Rewind();
             _uiBoxAnimation.Play();
             return true;
@@ -256,11 +270,12 @@ namespace Script.GameLogic
 
                 var size = max_y - min_y;
 
-                int currentYpos = _heightHandler.limitHeight + 1;
-                element.InitializationAfterGeneric(currentYpos);
-                element.myTransform.position = new Vector3(pos.x, pos.y + currentYpos - size, pos.z);
-                
                 RealizationBox.Instance.generator.SetRandomPositionForEvilBlox(element, usedPositions);
+                
+                int currentYpos = Random.Range( _matrix.MinHeightInCoordinates(element.blocks[0]._coordinates.x.ToIndex(), element.blocks[0]._coordinates.z.ToIndex()) + 1, _heightHandler.limitHeight + 5);
+                element.InitializationAfterGeneric(currentYpos);
+                element.myTransform.position = new Vector3(element.myTransform.position.x, pos.y + currentYpos - size, element.myTransform.position.z);
+                
                 ElementData.Instance.MergeElement(element);
                 
                 usedPositions.Add(element.blocks[0].xz);
@@ -268,8 +283,30 @@ namespace Script.GameLogic
                 Debug.Log("new block x: " + element.blocks[0].xz.x + " z: " + element.blocks[0].xz.z);
             }
 
+            BlockAnimations(blocks);
             OnAddBlock(blocks);
             _isOpenedBox = 0;
+        }
+
+        private void BlockAnimations(List<Block> blocks)
+        {
+            _blockCompleteCounter = blocks.Count;
+            foreach (var b in blocks)
+            {
+                var time = Random.Range(0, _timeBlockmove / 2);
+                b.transform.DOMove(b.transform.position, _timeBlockmove ).From(_centerPoint.position)
+                    .OnComplete(()=>OnBlockAnimationComplete());
+              //  b.transform.DOScale(b.transform.localScale, _timeBlockmove).From(Vector3.one * 0.3f);
+            }
+        }
+
+        public void OnBlockAnimationComplete()
+        {
+            _blockCompleteCounter--;
+            if (_blockCompleteCounter == 0)
+            {
+                OnOpenBoxEnded?.Invoke();
+            }
         }
         
         public void OnAddBlock(List<Block> pos)
@@ -302,6 +339,21 @@ namespace Script.GameLogic
                 DestroyParticle(box);
             }
             _boxes.Clear();
+        }
+        
+        
+        public void SetUIBox(Vector3 pos)
+        {
+            _evilBoxParentRectTransform.anchoredPosition = WorldToCanvas(pos);
+        }
+        
+        private Vector2 WorldToCanvas(Vector3 world_position)
+        {
+            var viewport_position = Camera.main.WorldToViewportPoint(world_position);
+            var canvas_rect = _canvas.GetComponent<RectTransform>();
+        
+            return new Vector2((viewport_position.x * canvas_rect.sizeDelta.x) - (canvas_rect.sizeDelta.x * 0.5f),
+                (viewport_position.y * canvas_rect.sizeDelta.y) - (canvas_rect.sizeDelta.y * 0.5f));
         }
     }
 }
