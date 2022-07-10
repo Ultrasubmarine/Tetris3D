f26b65b5148f0389616c8a8d2a69d96f5bd23dc7
sybmarina3000 <sybmarina3000@yandex.ru>
2022-07-10 16:43:11 +0300

    Cards panel animation

--- 3D_Tetris/Assets/Script/Home/ToglePanel.cs
+++ 3D_Tetris/Assets/Script/Home/ToglePanel.cs
@@ -19,18 +19,25 @@ namespace Script.Home
 
         [SerializeField] private CanvasGroup _leftCanvas;
         [SerializeField] private CanvasGroup _rightCanvas;
-
+        
+        [SerializeField] private RectTransform _cardPanel;
 
         bool _isLeft;
         
         private void Start()
         {
             _switchButton.onClick.AddListener(OnSwitch);
-            
+
+            _cardPanel.anchorMin = Vector2.one * 0.5f;
+            _cardPanel.anchorMax = Vector2.one * 0.5f;
+            _cardPanel.pivot = new Vector2(0.5f, 0.5f);
+            _cardPanel.sizeDelta = _cardPanel.parent.GetComponent<RectTransform>().rect.size;
+
             _leftAnimation = DOTween.Sequence().SetAutoKill(false).Pause();
             _leftAnimation
                 .Append(_animationTextCanvas.DOFade(0f, _time / 4).From(1f))
                 .Join(_rightCanvas.DOFade(0f, _time / 4))
+                .Join(_cardPanel.DOAnchorPosX(_cardPanel.sizeDelta.x/5, _time / 4).From(Vector2.zero))
                 .Append(_animationButton.DOAnchorPosX(-_animationButton.sizeDelta.x/2, _time / 2)
                     .From(Vector2.right * _animationButton.sizeDelta.x/2)
                     .OnComplete(() => _text.text = "home"))
@@ -44,6 +51,7 @@ namespace Script.Home
                 .Append(_animationButton.DOAnchorPosX(_animationButton.sizeDelta.x/2, _time / 2)
                     .From(Vector2.right * -_animationButton.sizeDelta.x/2)
                     .OnComplete(() => _text.text = "cards"))
+                .Join(_cardPanel.DOAnchorPosX(0, _time / 3).From(Vector2.right * _cardPanel.sizeDelta.x/5))
                 .Join(_rightCanvas.DOFade(1f, _time / 3).OnComplete(() => _rightCanvas.blocksRaycasts = true))
                 .Append(_animationTextCanvas.DOFade(1f, _time / 4).From(0f));
 
