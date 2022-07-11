bfbe6a8e7a02970437d3819452fb9dc886ccc1e4
sybmarina3000 <sybmarina3000@yandex.ru>
2022-07-11 14:13:50 +0300

    deleted preview in game scene;

--- 3D_Tetris/Assets/Script/Animations/ScreenChanger.cs
+++ 3D_Tetris/Assets/Script/Animations/ScreenChanger.cs
@@ -1,4 +1,5 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
@@ -15,12 +16,28 @@ public class ScreenChanger : MonoBehaviour
 
     private Sequence _animation;
 
+    [SerializeField] private bool _startWithHide;
+    [SerializeField] private bool _startWhenActive;
     private void Awake()
     {
         _animation = DOTween.Sequence().SetAutoKill(false).Pause();
 
-        _animation.Append(_screen.DOFade(1, _duration/2).From(0, false).OnComplete(Hide)).
-            Append(_screen.DOFade(0, _duration/2).From(1, false).OnComplete(EndChange));
+        if (_startWithHide)
+        {
+            _animation.Append(_screen.DOFade(0, _duration).From(1, false).OnPlay(Hide).OnComplete(EndChange));
+        }
+        else
+        {
+            _animation.Append(_screen.DOFade(1, _duration/2).From(0, false).OnComplete(Hide)).
+                Append(_screen.DOFade(0, _duration/2).From(1, false).OnComplete(EndChange));
+        }
+  
+    }
+
+    private void Start()
+    {
+        if(_startWhenActive)
+            Play();
     }
 
     public void Play()
